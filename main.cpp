f6e514ed8ca8f98c1eefd5c0ada286ee37a16c56
29anasr-coder <29anasr@wpsstudents.org>
2026-09-16 19:33:27 -0400

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,105 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #include <windows.h>\
+#include <exdisp.h>\
+#include <olectl.h>\
+#include <shlwapi.h>\
+\
+#pragma comment(lib, "shlwapi.lib")\
+\
+HINSTANCE hInst;\
+HWND hWndMain;\
+IWebBrowser2* pWebBrowser = NULL;\
+\
+void GetLocalHtmlPath(wchar_t* buffer, DWORD size) \{\
+    GetModuleFileNameW(NULL, buffer, size);\
+    PathRemoveFileSpecW(buffer);\
+    PathAppendW(buffer, L"index.html");\
+\}\
+\
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) \{\
+    switch (message) \{\
+    case WM_CREATE: \{\
+        OleInitialize(NULL);\
+        RECT rc;\
+        GetClientRect(hWnd, &rc);\
+\
+        HRESULT hr = CoCreateInstance(CLSID_WebBrowser, NULL, CLSCTX_INPROC_SERVER, IID_IWebBrowser2, (void**)&pWebBrowser);\
+        if (SUCCEEDED(hr)) \{\
+            IOleObject* pOleObject = NULL;\
+            pWebBrowser->QueryInterface(IID_IOleObject, (void**)&pOleObject);\
+            if (pOleObject) \{\
+                pOleObject->SetClientSite(NULL);\
+                pOleObject->DoVerb(OLEIVERB_INPLACEACTIVATE, NULL, NULL, 0, hWnd, &rc);\
+                pOleObject->Release();\
+            \}\
+\
+            wchar_t htmlPath[MAX_PATH];\
+            GetLocalHtmlPath(htmlPath, MAX_PATH);\
+\
+            BSTR bstrUrl = SysAllocString(htmlPath);\
+            VARIANT varEmpty;\
+            VariantInit(&varEmpty);\
+            pWebBrowser->Navigate(bstrUrl, &varEmpty, &varEmpty, &varEmpty, &varEmpty);\
+            SysFreeString(bstrUrl);\
+\
+            pWebBrowser->put_Visible(VARIANT_TRUE);\
+        \}\
+        break;\
+    \}\
+    case WM_SIZE: \{\
+        if (pWebBrowser) \{\
+            long width = LOWORD(lParam);\
+            long height = HIWORD(lParam);\
+            pWebBrowser->put_Width(width);\
+            pWebBrowser->put_Height(height);\
+        \}\
+        break;\
+    \}\
+    case WM_DESTROY:\
+        if (pWebBrowser) \{\
+            pWebBrowser->Release();\
+        \}\
+        OleUninitialize();\
+        PostQuitMessage(0);\
+        break;\
+    default:\
+        return DefWindowProc(hWnd, message, wParam, lParam);\
+    \}\
+    return 0;\
+\}\
+\
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) \{\
+    hInst = hInstance;\
+    \
+    WNDCLASSEXW wcex = \{ 0 \};\
+    wcex.cbSize = sizeof(WNDCLASSEX);\
+    wcex.style = CS_HREDRAW | CS_VREDRAW;\
+    wcex.lpfnWndProc = WndProc;\
+    wcex.hInstance = hInstance;\
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);\
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);\
+    wcex.lpszClassName = L"BennyXPClass";\
+\
+    RegisterClassExW(&wcex);\
+\
+    hWndMain = CreateWindowW(L"BennyXPClass", L"Raise A Benny", WS_OVERLAPPEDWINDOW,\
+        CW_USEDEFAULT, 0, 800, 600, NULL, NULL, hInstance, NULL);\
+\
+    if (!hWndMain) return FALSE;\
+\
+    ShowWindow(hWndMain, nCmdShow);\
+    UpdateWindow(hWndMain);\
+\
+    MSG msg;\
+    while (GetMessage(&msg, NULL, 0, 0)) \{\
+        TranslateMessage(&msg);\
+        DispatchMessage(&msg);\
+    \}\
+\
+    return (int)msg.wParam;\
+\}}
